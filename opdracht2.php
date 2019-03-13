d04d9ae68eed6dcc0c39fbca0b04d68e5f5f05d2
Melih <mredzhebov@roc-dev.com>
2019-03-13 08:47:43 +0100

    hoi

---
+++ opdracht2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Melih
+ * Date: 13-3-2019
+ * Time: 08:38
+ */
+
+$aantal = 14;
+$deze_jongen = "Deze jongen is ";
+
+
+
+echo $deze_jongen.$aantal.  "jaar oud ";
